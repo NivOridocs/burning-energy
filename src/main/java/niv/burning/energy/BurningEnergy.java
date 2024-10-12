71710b0bb4f2922af793e294c0c1910eb7ef1b90
Niv Oridocs <fradige95@gmail.com>
2024-10-12 12:49:11 +0200

    Register fallback providers

--- src/main/java/niv/burning/energy/BurningEnergy.java
+++ src/main/java/niv/burning/energy/BurningEnergy.java
@@ -1,24 +1,32 @@
 package niv.burning.energy;
 
-import net.fabricmc.api.ModInitializer;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.entity.AbstractFurnaceBlockEntity;
+import niv.burning.api.BurningStorage;
+import niv.burning.energy.config.Configuration;
+import team.reborn.energy.api.EnergyStorage;
+
 public class BurningEnergy implements ModInitializer {
-	public static final String MOD_ID = "burning-energy";
+    public static final String MOD_ID = "burning-energy";
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+    @Override
+    public void onInitialize() {
+        LOGGER.info("Initialize");
 
-		LOGGER.info("Initialize");
-	}
+        EnergyStorage.SIDED.registerFallback(new BurningEnergyFallback<>(
+                Configuration::enableEnergyStorageFallback, BurningStorage.SIDED, BurningFallbackEnergyStorage::new));
+
+        BurningStorage.SIDED.registerFallback(new BurningEnergyFallback<>(
+                Configuration::enableBurningStorageFallback, EnergyStorage.SIDED, EnergyFallbackBurningStorage::new));
+    }
+
+    public static final int getEnergyDuration(ItemStack stack) {
+        return AbstractFurnaceBlockEntity.getFuel().getOrDefault(stack.getItem(), 0) * 5 / 2;
+    }
 }
